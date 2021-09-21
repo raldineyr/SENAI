14a5ee85a24e5f5cf734e65e54597f2e6bf95810
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-09-21 16:38:03 -0300

    Create L01I.c

---
+++ logicaDeProgramacao/L01I.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+ int A, B, quadradoDaDiferenca;
+
+printf("Informe o A: ");
+ scanf("%d", &A);
+
+printf("Informe o B: ");
+ scanf("%d", &B);
+
+quadradoDaDiferenca = (A - B);
+
+printf("O quadrado da diferença é: %d ",quadradoDaDiferenca * quadradoDaDiferenca);
+
+  return 0;
+}
