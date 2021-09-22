c42953f2ab57eb8b7f19b5b932c64585d902b161
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-09-22 08:29:00 -0300

    Create L01L.c

---
+++ logicaDeProgramacao/L01L.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+
+int A, B, C, somaDosQuadrados;
+
+printf("Informe respectivamente A, B e C: ");
+
+scanf("%d %d %d",&A, &B, &C);
+
+somaDosQuadrados = (A*A)+(B*B)+(C*C);
+
+printf("A soma dos quadrados dos valores são: %d",somaDosQuadrados);
+
+  return 0;
+}
