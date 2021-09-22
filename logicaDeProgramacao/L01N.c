2b998ce6e729da4eaddf066632d58a83a72fc119
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-09-22 08:29:57 -0300

    Create L01N.c

---
+++ logicaDeProgramacao/L01N.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* Elaborar um programa de computador que efetue a leitura de quatro valores inteiros (variáveis A, B, C e D).
+
+Ao final o programa deve apresentar o resultado do produto (variável P) do: primeiro com o terceiro valor, e o resultado do produto (variável P) do primeiro com o terceiro valor, e o resultado da soma (variável S) do segundo com o quarto valor.*/
+
+int main(void) {
+  int A, B, C, D, P, S;
+  printf("Digite os 4 valores respectivamente: ");
+  scanf("%d %d %d %d", &A, &B, &C, &D);
+
+  printf("Resultado do produto A * C: %d\n",P = A * C);
+  printf("Resultado do produto A * C: %d\n", P = A * C); // O enunciado repete os valores.
+  printf("Resultado da adição A + C: %d\n", S = B + D);
+
+  return 0;
+}
