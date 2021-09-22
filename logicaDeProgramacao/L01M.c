3fe0f818f49aa51e42a1dc8c54e0895121daeff9
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-09-22 08:29:33 -0300

    Create L01M.c

---
+++ logicaDeProgramacao/L01M.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+
+int A, B, C, quadradoDaSoma;
+
+printf("Informe respectivamente A, B e C: ");
+
+scanf("%d %d %d",&A, &B, &C);
+
+  quadradoDaSoma = A + B + C;
+
+printf("O quadrado da soma é: %d",quadradoDaSoma * quadradoDaSoma);
+
+  return 0;
+}
